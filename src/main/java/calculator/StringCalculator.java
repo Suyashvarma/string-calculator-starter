e5229a4045e285e95edb2b0d282aaa975f1df835
Suyashvarma <88928225+Suyashvarma@users.noreply.github.com>
2021-10-17 23:27:02 +0530

    first point committed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,14 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	String Array[]=input.split(","); 
+    	if(input.equals(""))
         return 0;
+    	else if(input.length()==1)
+    	return Integer.parseInt(input);
+    	else 
+    		return Integer.parseInt(Array[0])+Integer.parseInt(Array[1]);
+    	
     }
 
 }
